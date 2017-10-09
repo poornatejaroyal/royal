1e473f897ae1d0ee7c6560a03f15c0f7b83c3361
mahesh0790 <mahesh.rockz790@gmail.com>
2017-10-09 10:14:05 +0530

    changes happen in hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ public class hello{
  
       public static void main(String[] args){
 
-            for(int i=1;i<=10;i++){
+            if(i=10)
+{
 
                     System.out.println("HELLO WORLD...........");
 
